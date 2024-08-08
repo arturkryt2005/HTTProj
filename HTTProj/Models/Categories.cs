dd9f94456f54c514edd4416ec8b8170963ada32d
Артур <125348508+arturkryt2005@users.noreply.github.com>
2024-08-08 13:15:48 +0300

    Настроил подключение к БД SSMS, т.е. создал модели данных, контекст, написал строку подключения, добавил сервис в Program.cs. Выполнил миграции.

---
+++ HTTProj/Models/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace HTTProj.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
